ce02a5ab97c7b63fa4164bd0c9cba5b52bdf2467
Sam <scook@hep.ucl.ac.uk>
2011-02-24 15:10:00 +0900

    General work on adcToPython, have working peak locator (using Tspectrum) but not yet able to fit _all_ peaks w/ one sum of gauses trying to recreate the ROOT multifit macro

--- adcToPhoton.py
+++ adcToPhoton.py
@@ -7,78 +7,135 @@ S. Cook 23-2-11
 """
 
 from ROOT import *
-from time import sleep
-
+from collections import OrderedDict
 
 def main():
-    ped_n = (70,)# ( 66,  71)#,  70,  78) #(272,  66,  71,  78)
-    dat_n = (277, )# (274, 278)#, 277, 286) #(273, 274, 278, 286)
-    ped_file_fmt = len(dat_n)*("data/pedestal_%03i.txt", ) #("data/test_%03i.txt",) + 3*("data/pedestal_%03i.txt", )
+    # TODO abstract this whole mess to allow processing of arbitary numbers of files etc
+    ped_n = 70 
+    dat_n = 277
+    min_bin = 0
+    max_bin = 4096
+    bin_merge = 2 # one bin ADC = one bin histo
+    
+    ped_file_fmt = "data/pedestal_%03i.txt"
     dat_file_fmt = "data/test_%03i.txt"
-    hist = []
+    n_bins = int((max_bin - min_bin)/bin_merge)
+    h = TH1D("d", "d", n_bins, min_bin, max_bin)
+    # store the data in a dictionary with the key being the bin
+    # this allows data to act as a sparse array
     
-    for i in dat_n: hist.append(TH1F("dark current%i" % i, "dark current", 1024, 0, 4096))#1024, 0, 4096)) #
-           
+    # read in the pedestal data and use the mean as the value
+    # TODO change this to a gaus fit on the peak
+    sum = count = 0
+    with open(ped_file_fmt % ped_n, "r") as file_ped:
+        for line in file_ped:
+            if '-' in line: continue
+            count += 1
+            sum += float(line.split()[0])
+    pedestal = int(round(sum / count))
     
-    for entry in zip(dat_n, ped_n, hist, ped_file_fmt):
-        ped_f = entry[3] % entry[1]
-        sum = count = 0
-        with open(ped_f, "r") as f:
-            for line in f:
-                if not ('-' in line): 
-                    sum    = sum + float(line.split()[1])
-                    count += 1
-        ped = sum/count
+    with open(dat_file_fmt % dat_n, "r") as file_dat:
+        for line in file_dat:
+            if '-' in line: continue
+            # maintain bin values as integers
+            val = int(line.split()[0]) - pedestal
+            h.Fill(val)
         
-        dat_f = dat_file_fmt % entry[0]
-        with open(dat_f, "r") as f:
-            for line in f:
-                if not ('-' in line): 
-                    val = float(line.split()[1]) - ped
-                    entry[2].Fill(val)
-            
-    first = True
-    i = 1
-    # quick hack as hist[2] is largest & needs to be drawn first
-    hist = (hist[0],)#, hist[1])# hist[3],) #hist[2],
-    for h in hist:
-        if first: 
-            h.Draw()
-            first = False
-            i += 1
-        else:
-            h.SetLineColor(i)
-            h.Draw("SAME")
-            i += 1
-          
-    spec = TSpectrum(10, 1) # max n peaks, sigma resolution        
-    spec.Search(hist[0], 1, "goff", 0.1)
-    n_pts = spec.GetNPeaks()
-    g =  spec.GetPositionX()
-    
-    # python can cope with c floats but must be referenced using c style 
-    for i in range(n_pts):
-            print g[i]
+    print peak_fit(h, n_bins, min_bin, max_bin)
+    h.Draw()
     stall()
-    # sleep(1)
+
+def find_peaks(data, resolution = 20, sparsity_stop = 3, thrs = 4):
+    # print data
+    h_res = int(resolution/2)
+    current_x = [] # lists the bins that are in use
+    current_y = []
+    grad = []
+    res = []
+    for bin in sorted(data.keys()):
+        if bin < 0: continue # skip underflow
+        
+        if len(current_x) < resolution:
+            # fill the current_x 
+            current_x.append(bin)
+            current_y.append(data[bin])
+            grad.append(0)
+        else:
+            current_x.pop(0)
+            current_x.append(bin)
+            current_y.pop(0)
+            current_y.append(data[bin])
+            
+            grad.pop(0)
+            dx = (sum(current_x[:h_res])/h_res) - (sum(current_x[h_res:])/h_res)
+            dy = (sum(current_y[:h_res])/h_res) - (sum(current_y[h_res:])/h_res) + 0.000001
+            grad.append(dx/dy)
+            if (current_x[-1] - current_x[0]) > resolution+sparsity_stop: 
+                print "too sparse stopping, bin:", bin
+                break
+            
+            # peak found!
+            if (sum(grad[:h_res]) > thrs and sum(grad[h_res:]) < -thrs ):#-0.05 < dx/dy < 0.05:
+                res.append(current_x[h_res])#res.append(bin)
+    return res
+            
     
-    for h in hist:
-        h = None
 
-def peak_check():
-    """iteratively to a Tspectrum search then
-    fit a gaussian to each found peak then remove that peak
-    logging it's centre and effective range before repeating
-    hopefully ths will then locate _all_ the peaks (including 
-    masked by other peaks) these peaks will then be used as 
-    locations & ranges of photon numbers for conversion"""
-    pass
 
+def peak_fit(hist, n_bins, min_bin, max_bin):
+    spectrum = TSpectrum(10, 1)
+    n_peaks = spectrum.Search(hist, 4, "goff nobackground", 0.15)
+    peaks = []
+    tmp_pos = spectrum.GetPositionX()
+    peaks = cArrayToList(tmp_pos, n_peaks)
+    print peaks
+    parameters = []
+    all_func = ''
+    working_hist = [hist.Clone("working"),]
+    # working_hist[-1].Copy(hist)
+    # working_hist[-1].Draw()
+    for i in range(n_peaks):
+        prev_peak = peaks[i - 1] if (i != 0) else min_bin
+        curr_peak = peaks[i]
+        next_peak = peaks[i + 1] if (i != len(peaks) - 1) else max_bin
+        
+        lBound = (prev_peak + curr_peak)/2 if (prev_peak != min_bin) else min_bin
+        uBound = (curr_peak + next_peak)/2 if (next_peak != max_bin) else max_bin
+        
+        func_name = "f%i"%i
+        fit_f = TF1(func_name, "gaus", lBound, uBound) #min_bin, max_bin)#
+        working_hist[-1].Fit(func_name, "QR")#hist.Fit(func_name, "R+")
+        
+        working_hist.append(working_hist[-1].Clone("working%i"%i))
+        working_hist[-1].Add(fit_f, -1) # subtract the previous peak as background for the next one
+        
+        parameters.append(cArrayToList(fit_f.GetParameters(), 3))
+        all_func += 'gaus(%i)+' % (3*i) if (i < len(peaks) - 1) else 'gaus(%i)' % (3*i)
+    
+    print parameters
+    print all_func
+    fit_all = TF1("all", all_func, min_bin, max_bin)
+    for i in range(n_peaks):
+        for par in range (3): 
+            fit_all.SetParameter(par, parameters[i][par])
+        
+    hist.Draw()
+    # print "name", hist.GetName()
+    hist.Fit(fit_all, "QR+")
+    return cArrayToList(fit_all.GetParameters(), 3*n_peaks)
+
+def cArrayToList(array, len):
+    res = []
+    for i in range(len):
+        res.append(array[i])
+    return res
 
 def stall():
+    from time import sleep
     while True:
         try:
-            sleep(100)
+            sleep(60)
+            exit()
         except KeyboardInterrupt:
             break
 
---
+++ multiFit.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+multiFit.py
+
+Created by Sam Cook on 2011-02-24.
+Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
